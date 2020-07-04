2c589020ec830c48db01e5a71f66b31bc9ac1ff9
harishbalasubramanian <harishbala153@gmail.com>
2020-07-04 11:30:34 -0700

    This is the Code.

---
+++ Code.java
@@ -0,0 +1,118 @@
+import java.util.*;
+public class Code {
+	int g = 10;
+	public static void main(String[] args) {
+//		Apple a1 = new Apple(5,"novel apple","red");
+//		Apple a2 = new Apple("apple species 2",6,"green");
+//		System.out.println(a2.width);
+//		System.out.println(a2.species);
+//		System.out.println(a2.color);
+//		Apple a3 = new Apple();
+//		Scanner sc = new Scanner(System.in);
+//		House h = new House(sc.nextInt(), sc.next(),sc.nextInt(),sc.nextInt());
+//		System.out.println(h.squarefeet);
+//		System.out.println(h.color);
+//		System.out.println(h.numFurniture);
+//		System.out.println(h.floors);
+//		Earth e = new Earth(10,"Earth",50, 3000, 10);
+//		//System.out.println(e.quantityOfLand);
+//		e.moreHumans();
+//		System.out.println(e.numberOfHumans);
+	}
+}
+//class Apple{
+//	String color;
+//	String species;
+//	int width;
+//	public Apple(int w, String s, String c) {
+//		width = w;
+//		color = c;
+//		species = s;
+//		System.out.println("Constructor 1");
+//	}
+//	public Apple(String s, int w, String c) {
+//		width = w;
+//		color = c;
+//		species = s;
+//		System.out.println("Constructor 2");
+//	}
+//	public Apple() {
+//		width = 4;
+//		species = "hello";
+//		color = "blue";
+//	}
+//	public void pluck() {
+//		System.out.println("PLUCKED");
+//	}
+//}
+//class House{
+//	int squarefeet;
+//	String color;
+//	int numFurniture;
+//	int floors;
+//	public House(int squarefeet, String color, int numFurniture, int floors) {
+//		this.squarefeet = squarefeet;
+//		this.color = color;
+//		this.numFurniture = numFurniture;
+//		this.floors = floors;
+//	}
+//	public void method() {
+//		int s = 5;
+//	}
+//	public void method2() {
+//		int q = 3;
+//	}
+//}
+//class A{
+//	int b;
+//	int c;
+//	int d;
+//	public void method() {
+//		int var = 0;
+//		var = 20;
+//	}
+//	public void method2() {
+//		b = 10;
+//	}
+//}
+//class Earth{
+//	int quantityOfWater;
+//	String name;
+//	int quantityOfLand;
+//	int numberOfHumans;
+//	int numberOfTrees;
+//	public Earth(int quantityOfWater, String name, int quantityOfLand, int numberOfHumans, int numberOfTrees) {
+//		this.quantityOfWater = quantityOfWater;
+//		this.name = name;
+//		this.quantityOfLand = quantityOfLand;
+//		this.numberOfHumans = numberOfHumans;
+//		this.numberOfTrees = numberOfTrees;
+//	}
+//	public void growTrees() {
+//		System.out.println("Trees are growing");
+//		numberOfTrees++;
+//	}
+//	public void moreHumans() {
+//		System.out.println("There are more humans");
+//		numberOfHumans++;
+//	}
+//}
+//class School{
+//	int numTeachers;
+//	int numBuildings;
+//	String name;
+//	int numStudents;
+//	public School(int numTeachers, int numBuildings, String name, int numStudents) {
+//		this.numTeachers = numTeachers;
+//		this.numBuildings = numBuildings;
+//		this.name = name;
+//		this.numStudents = numStudents;
+//	}
+//	public void hireTeachers() {
+//		System.out.println("More teachers are hired");
+//		numTeachers++;
+//	}
+//	public void moreStudentsEnrolled() {
+//		System.out.println("More students are enrolled");
+//	}
+//}
